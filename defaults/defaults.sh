5ac2c4c96616b4f3e64ac7dd93fee405e5c7309b
Aleem Haji <hajial@gmail.com>
2016-01-05 22:44:06 -0500

    Added in a bunch of settings that probably aren't defaults.

--- defaults/defaults.sh
+++ defaults/defaults.sh
@@ -7,4 +7,18 @@ defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
 # Enable tabby spacey keyboard stuff
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
+# Trackpad tracking speed
+defaults write NSGlobalDomain com.apple.trackpad.scaling -float 0.6875
+
+# Dock on left
+defaults write com.apple.dock orientation -string left
+
+# Wait 1 minute before locking when screensaver
+defaults write com.apple.screensaver askForPasswordDelay -float 60
+
+# Strange key name, but enable zooming with control + scroll
+defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
+
+killall Dock
+
 echo "Some settings have been updated. But you'll have to log out and back in again for some settings to apply."

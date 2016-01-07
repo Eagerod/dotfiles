476379ffb572880ee2a3a759c706b4c353b343f6
Aleem Haji <hajial@gmail.com>
2016-01-06 23:22:54 -0500

    "Cleaned it up"? I dunno. The function is nice, but it's just all ugly.

--- rsync/backup.sh
+++ rsync/backup.sh
@@ -8,48 +8,48 @@ fi
 ip_address=$1
 export RSYNC_PASSWORD=$2
 
-dst_root="rsync://rsync@$ip_address"
-rsync_params="-ahuDH --progress"
+do_backup() {
+    rsync -ahuDH --progress --exclude-from=.rsync_exclude "$1" "rsync://rsync@$ip_address:$2"
+}
 
 echo "*.DS_Store" > ".rsync_exclude"
 echo "*/.git/*" >> ".rsync_exclude"
 echo "*.rsync_pass" >> ".rsync_exclude"
 echo ".rsync_exclude" >> ".rsync_exclude"
-rsync_params="$rsync_params --exclude-from=.rsync_exclude"
 
 # Sync Mac EVE to NAS
-# rsync $rsync_params "/Applications/EVE Online.app" $dst_root:/backups/
+# do_backup "/Applications/EVE Online.app" "/backups/"
 
 # Sync Steam Libraries to NAS
 clear; echo "Syncing Steam library"
-rsync $rsync_params "/Users/Aleem/Library/Application Support/Steam/SteamApps/" $dst_root:/backups/Mac\ SteamApps
+do_backup "/Users/Aleem/Library/Application Support/Steam/SteamApps/" "/backups/Mac SteamApps"
 
 # Sync Photo Libraries to NAS
 clear; echo "Syncing photo library from both directories"
-rsync $rsync_params "/Users/Aleem/Pictures/iPhoto Games Screenshots Library.photolibrary" $dst_root:/backups/Photos/
-rsync $rsync_params "/Users/Aleem/Pictures/iPhoto Library" $dst_root:/backups/Photos/
+do_backup "/Users/Aleem/Pictures/iPhoto Games Screenshots Library.photolibrary" "/backups/Photos/"
+do_backup "/Users/Aleem/Pictures/iPhoto Library" "/backups/Photos/"
 
 # Sync iPhone backups
 clear; echo "Syncing iPhone backups"
-rsync $rsync_params "/Users/Aleem/Library/Application Support/MobileSync/Backup/" $dst_root:/backups/iPhone\ Backups
+do_backup "/Users/Aleem/Library/Application Support/MobileSync/Backup/" "/backups/iPhone Backups"
 
 # Sync Home Directory Contents to NAS
 clear; echo "Syncing ~ directories"
-rsync $rsync_params "/Users/Aleem/Documents" $dst_root:/backups/
-rsync $rsync_params "/Users/Aleem/Desktop/" $dst_root:/backups/MacDesktop
-rsync $rsync_params "/Users/Aleem/Dropbox" $dst_root:/backups/
+do_backup "/Users/Aleem/Documents" "/backups/"
+do_backup "/Users/Aleem/Desktop/" "/backups/MacDesktop"
+do_backup "/Users/Aleem/Dropbox" "/backups/"
 
 # Sync up old Xcode Archives so that they can be cleared out from this system.
 clear; echo "Syncing old Xcode Archives"
-rsync $rsync_params "/Users/Aleem/Library/Developer/Xcode/Archives" $dst_root:/backups/XcodeArchives
+do_backup "/Users/Aleem/Library/Developer/Xcode/Archives" "/backups/XcodeArchives"
 
 # If the external hard drive is plugged in, sync a bunch of shit from there.
 if [ -d /Volumes/Games ]; then
 	
-	rsync $rsync_params "/Volumes/Games/EVE" $dst_root:/backups/
+	do_backup "/Volumes/Games/EVE" "/backups/"
 	# Don't sync Guild Wars because the asshole developers wrapped up like 16 gigs into a single file. 
-	# rsync $rsync_params "/Volumes/Games/Guild Wars 2" $dst_root:/backups/
-	# rsync $rsync_params "/Volumes/Games/Program Files \(x86\)/Steam" $dst_root:/backups/
+	# do_backup "/Volumes/Games/Guild Wars 2" "/backups/"
+	# do_backup "/Volumes/Games/Program Files \(x86\)/Steam" "/backups/"
 
 fi
 

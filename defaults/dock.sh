0c6cf0f2d89153ff1eac8bb94f19b9a601979f73
Aleem Haji <hajial@gmail.com>
2016-01-07 22:57:01 -0500

    Updated dock file in preparation for adding in directories.

--- defaults/dock.sh
+++ defaults/dock.sh
@@ -2,7 +2,7 @@ clear_dock() {
     defaults write com.apple.dock persistent-apps -array
 }
 
-add_item_to_dock() {
+add_app_to_dock() {
     fileroot=$1
     filename=$2
     filepath="file://"`find $1 -maxdepth 2 -iname "$2"`
@@ -14,28 +14,28 @@ add_item_to_dock() {
     defaults write com.apple.dock persistent-apps -array-add "<dict>$tile_data$tile_type</dict>"
 }
 
-add_spacer_to_dock() {
+add_app_spacer_to_dock() {
     defaults write com.apple.dock persistent-apps -array-add '{tile-data={}; tile-type="spacer-tile";}'
 }
 
 clear_dock
 
 # Always on apps.
-add_item_to_dock "/opt/homebrew-cask/Caskroom/spotify" "Spotify.app"
-add_item_to_dock "/Applications" "Messages.app"
-add_item_to_dock "/opt/homebrew-cask/Caskroom/sublime-text" "Sublime Text 2.app"
-add_item_to_dock "/Applications" "Terminal.app"
+add_app_to_dock "/opt/homebrew-cask/Caskroom/spotify" "Spotify.app"
+add_app_to_dock "/Applications" "Messages.app"
+add_app_to_dock "/opt/homebrew-cask/Caskroom/sublime-text" "Sublime Text 2.app"
+add_app_to_dock "/Applications" "Terminal.app"
 if type charm > /dev/null 2> /dev/null; then
-    add_item_to_dock "/opt/homebrew-cask/Caskroom/pycharm" "PyCharm.app"
+    add_app_to_dock "/opt/homebrew-cask/Caskroom/pycharm" "PyCharm.app"
 fi
 
-add_spacer_to_dock
+add_app_spacer_to_dock
 
 # Usually on apps.
-add_item_to_dock "/Applications" "Google Chrome.app"
-add_item_to_dock "/opt/homebrew-cask/Caskroom/slack" "Slack.app"
+add_app_to_dock "/Applications" "Google Chrome.app"
+add_app_to_dock "/opt/homebrew-cask/Caskroom/slack" "Slack.app"
 
-add_spacer_to_dock
+add_app_spacer_to_dock
 
 # All transient apps go below this spacer.
 

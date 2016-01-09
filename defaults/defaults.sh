56f3c3c0e8f4d80b41a316dd3e895d022e14c2cc
Aleem Haji <hajial@gmail.com>
2016-01-09 01:27:53 -0500

    Consistency a bunch of things.

--- defaults/defaults.sh
+++ defaults/defaults.sh
@@ -20,6 +20,15 @@ defaults write com.apple.screensaver askForPasswordDelay -float 60
 defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
 defaults write com.apple.universalaccess closeViewSmoothImages -bool true
 
+# Live in the matrix
+defaults write com.apple.Terminal 'Startup Window Settings' -string Homebrew
+defaults write com.apple.Terminal 'Default Window Settings' -string Homebrew
+
+# Nested things that are tough to get with `defaults`
+/usr/libexec/PlistBuddy -c "set DesktopViewSettings:IconViewSettings:iconSize 128" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "set DesktopViewSettings:IconViewSettings:gridSpacing 72" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "set DesktopViewSettings:IconViewSettings:arrangeBy dateModified" ~/Library/Preferences/com.apple.finder.plist
+
 killall Dock
 
 echo "Some settings have been updated. But you'll have to log out and back in again for some settings to apply."

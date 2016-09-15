afdbaeff8aae24c92588ad3c2558f6892a5bb0a2
Aleem Haji <hajial@gmail.com>
2016-09-15 09:21:50 -0400

    Added a script that might soon be used to create sublime dictionary entries, since they appear to be case sensitive.

---
+++ sublime/ssre.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env sh
+#
+# Super Simple Regular Expression Expander.
+#
+# Takes a very limited subset of regular expressions and expands out possible choices.
+
+append_character_to_all() {
+    if [ $# -eq 1 ]
+    then   
+        echo "$1" 
+    else
+        local character=$1
+        for i in $(seq 2 $#); 
+        do     
+           echo "${!i}$character" 
+        done
+    fi
+}
+
+duplicating=false
+
+while read regular_expression 
+do
+    all_wordset=()
+    current_duplication_wordset=()
+    for i in $(seq 0 $((${#regular_expression}-1))) 
+    do
+        character=${regular_expression:$i:1}
+        if [ $character = "[" ] 
+        then
+            # Duplicate entire word_builder for every character until "]"
+            duplicating=true
+            current_duplication_wordset=("${all_wordset[@]}")
+            all_wordset=()
+        elif [ $character = "]" ]
+        then
+            duplicating=false
+            current_duplication_wordset=("${all_wordset[@]}")
+        elif [ $duplicating = true ]
+        then
+            all_wordset+=($(append_character_to_all $character ${current_duplication_wordset[@]}))
+        elif [ $duplicating = false ]
+        then
+            all_wordset=($(append_character_to_all $character ${all_wordset[@]}))
+        fi
+    done
+    echo ${all_wordset[@]}
+done

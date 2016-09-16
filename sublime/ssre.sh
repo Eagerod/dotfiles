d5d97e6c72e4335624b11698900ac0f9ea0b5d5a
Aleem Haji <hajial@gmail.com>
2016-09-16 09:37:49 -0400

    Refactored a bit, so I can scope things better.

--- sublime/ssre.sh
+++ sublime/ssre.sh
@@ -6,28 +6,35 @@
 # Will (obviously) never work with regular expressions containing non-finitely repeating characters.
 # Will probably not support very mechanisms that exist in regular expressions.
 #
+# Mechanisms that can probably not be supported:
+# - Non-Finite repeating sequences - *, +
+#
 # Currently supported mechanisms:
 # - Character Sets
+#
+# Things to support when the need arises:
+# - Optional sequences - ?
+# - Finite repeating sequences - {n,m}
+# - Ors - |
 
-append_character_to_all() {
+append_word_to_all() {
     if [ $# -eq 1 ]
     then   
         echo "$1" 
     else
-        local character=$1
+        local word=$1
         for i in $(seq 2 $#); 
         do     
-           echo "${!i}$character" 
+           echo "${!i}$word" 
         done
     fi
 }
 
-duplicating=false
-
-while read regular_expression 
-do
-    all_wordset=()
-    current_duplication_wordset=()
+expand_regular_expression() {
+    local regular_expression="$1"
+    local all_wordset=()
+    local current_duplication_wordset=()
+    local duplicating=false
     for i in $(seq 0 $((${#regular_expression}-1))) 
     do
         character=${regular_expression:$i:1}
@@ -43,14 +50,19 @@ do
             current_duplication_wordset=("${all_wordset[@]}")
         elif [ $duplicating = true ]
         then
-            all_wordset+=($(append_character_to_all $character ${current_duplication_wordset[@]}))
+            all_wordset+=($(append_word_to_all $character ${current_duplication_wordset[@]}))
         elif [ $duplicating = false ]
         then
-            all_wordset=($(append_character_to_all $character ${all_wordset[@]}))
+            all_wordset=($(append_word_to_all $character ${all_wordset[@]}))
         fi
     done
     for word in ${all_wordset[@]}
     do
         echo $word
     done
+}
+
+while read regular_expression
+do
+    expand_regular_expression "$regular_expression"
 done

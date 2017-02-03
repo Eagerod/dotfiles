36f2cbda3ed4918358d0cfa4162efbd669f09b1e
Aleem Haji <hajial@gmail.com>
2017-02-03 01:49:12 -0500

    Added Chrome to the list of things to install on linux.

--- apt/apt.sh
+++ apt/apt.sh
@@ -35,4 +35,9 @@ mv ngrok /usr/local/bin/
 cd ..
 rm -rf ngrok-unzip
 
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt-get install -f
+rm google-chrome-stable_current_amd64.deb
+
 sudo -k

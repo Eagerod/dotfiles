7d291d33a804e850d3c1c429ae109325321ce999
Aleem Haji <hajial@gmail.com>
2017-02-07 23:48:33 -0500

    Install later version of mongodb.

--- apt/apt.sh
+++ apt/apt.sh
@@ -9,6 +9,10 @@ echo "Installing a bunch of packages, will likely require permissions escalation
 # Repositories where all these different packages lie.
 sudo add-apt-repository "deb https://cli-assets.heroku.com/branches/stable/apt ./"
 
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+
 curl -sL https://cli-assets.heroku.com/apt/release.key | sudo apt-key add -
 
 sudo apt-get update
@@ -16,7 +20,7 @@ sudo apt-get update
 sudo apt-get install git
 sudo apt-get install heroku
 sudo apt-get install nodejs-legacy
-sudo apt-get install mongodb
+sudo apt-get install mongodb-org
 sudo apt-get install texlive-full
 sudo apt-get install xclip
 

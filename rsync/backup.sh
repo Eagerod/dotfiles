e7716bee0ae43ca81734825b9e728524c39344e3
Aleem Haji <hajial@gmail.com>
2017-04-14 13:51:33 -0400

    Removed the clear lines from rsync backup script, cause it was actually getting pretty annoying.

--- rsync/backup.sh
+++ rsync/backup.sh
@@ -18,25 +18,25 @@ echo "*/.git/*" >> ".rsync_exclude"
 echo ".rsync_exclude" >> ".rsync_exclude"
 
 # Sync Steam Libraries to NAS
-clear; echo "Syncing Steam library"
+echo "Syncing Steam library"
 do_backup "$HOME/Library/Application Support/Steam/SteamApps/" "/backups/Mac SteamApps"
 
 # Sync Photo Libraries to NAS
-clear; echo "Syncing photo library from both directories"
+echo "Syncing photo library from both directories"
 do_backup "$HOME/Pictures/iPhoto Library" "/backups/Photos/"
 
 # Sync iPhone backups
-clear; echo "Syncing iPhone backups"
+echo "Syncing iPhone backups"
 do_backup "$HOME/Library/Application Support/MobileSync/Backup/" "/backups/iPhone Backups"
 
 # Sync Home Directory Contents to NAS
-clear; echo "Syncing ~ directories"
+echo "Syncing ~ directories"
 do_backup "$HOME/Documents" "/backups/"
 do_backup "$HOME/Desktop/" "/backups/MacDesktop"
 do_backup "$HOME/Dropbox" "/backups/"
 
 # Sync up old Xcode Archives so that they can be cleared out from this system.
-clear; echo "Syncing old Xcode Archives"
+echo "Syncing old Xcode Archives"
 do_backup "$HOME/Library/Developer/Xcode/Archives" "/backups/XcodeArchives"
 
 rm ".rsync_exclude"

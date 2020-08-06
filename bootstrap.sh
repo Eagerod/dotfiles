8163495952deedb7d9aaf1d234bdaaaa4aa958e6
Aleem Haji <hajial@gmail.com>
2020-08-05 21:16:15 -0400

    Allow the bootstrap script run from any directory.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,26 +1,35 @@
 #!/usr/bin/env bash
+#
+# Get this machine up to snuff
+set -e
 
-if [ "$(uname)" == "Darwin" ]; then
-	sh brew/brew.sh
+SCRIPT_DIR="$(cd $(dirname "$0") && pwd)"
 
-	sh defaults/dock.sh
-	sh defaults/defaults.sh
+UNAME="$(uname)"
 
-	duti file-associations/duti
+echo >&2 "Setting up a $UNAME machine..."
+
+if [ "$UNAME" == "Darwin" ]; then
+	sh "$SCRIPT_DIR/brew/brew.sh"
+
+	sh "$SCRIPT_DIR/defaults/dock.sh"
+	sh "$SCRIPT_DIR/defaults/defaults.sh"
+
+	duti "$SCRIPT_DIR/file-associations/duti"
 else
-    sh apt/apt.sh
-    sh file-associations/gnome.sh
+    sh "$SCRIPT_DIR/apt/apt.sh"
+    sh "$SCRIPT_DIR/file-associations/gnome.sh"
 fi
 
-bash bash/bash.sh
-bash git/git.sh
-bash vim/vim.sh
+bash "$SCRIPT_DIR/bash/bash.sh"
+bash "$SCRIPT_DIR/git/git.sh"
+bash "$SCRIPT_DIR/vim/vim.sh"
 
-bash sublime/sublime.sh
+bash "$SCRIPT_DIR/sublime/sublime.sh"
 
 if [ "$(stat -c "%U" /usr/local/bin)" != "$(whoami)" ]; then
     echo "/usr/local/bin not writeable, please provide permissions:"
     sudo chown $(whoami) /usr/local/bin
 fi
 
-bash bin/bin.sh
+bash "$SCRIPT_DIR/bin/bin.sh"

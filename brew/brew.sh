c3eb8bea52160340c66a3a9f92a72df166e1e7e6
Aleem Haji <hajial@gmail.com>
2020-08-05 21:24:10 -0400

    Update brew file to not install things that would be run in containers these days.
    Drops whatever things I tend to use less as well.

--- brew/brew.sh
+++ brew/brew.sh
@@ -7,28 +7,21 @@ fi
 brew update
 
 brew install git
-brew install heroku-toolbelt
 brew install nodejs
-brew install mongodb
 brew install duti
-brew install --with-rlwrap repl
-# brew install boot2docker # Only if doing docker stuff.
+brew install docker-machine docker docker-compose
 
-brew cask install ngrok
 brew cask install sublime-text
-brew cask install dropbox
 brew cask install google-chrome
 brew cask install virtualbox
-brew cask install skype
 brew cask install slack
 brew cask install spotify
 brew cask install steam
 brew cask install vlc
-brew cask install mactex
 brew cask install transmission
 brew cask install flux
 brew cask install istat-menus
 brew cask install alfred
 
 # For anything that was already installed.
-brew upgrade --all
+brew upgrade

53b856de3211f8a753013739cd12d2cac1bce648
Aleem Haji <hajial@gmail.com>
2020-10-01 12:58:46 -0400

    Removed old git completion downloader.

--- git/git.sh
+++ git/git.sh
@@ -8,16 +8,3 @@ DEST_DIR=$HOME
 
 create_symlink_and_backup "$SCRIPT_DIR/gitconfig" "$DEST_DIR/.gitconfig"
 create_symlink_and_backup "$SCRIPT_DIR/gitignore" "$DEST_DIR/.gitignore"
-
-# Intentionally update, in case git was updated since the last time this was run.
-git_version="v$(git version | awk '{print $NF }')"
-
-# For some reason, curl in cygwin on windows can have issues writing using -o,
-#   so instead of having curl write the file, redirect it to its end path.
-if uname | grep -iq cygwin; then
-    curl https://raw.githubusercontent.com/git/git/$git_version/contrib/completion/git-completion.bash > ~/.git-completion.bash
-    curl https://raw.githubusercontent.com/git/git/$git_version/contrib/completion/git-prompt.sh > ~/.git-prompt.sh
-else
-    curl https://raw.githubusercontent.com/git/git/$git_version/contrib/completion/git-completion.bash -o ~/.git-completion.bash
-    curl https://raw.githubusercontent.com/git/git/$git_version/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
-fi

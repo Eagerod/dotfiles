130991ce1e9b90f3625a8e60e0d31845e489f8cb
Aleem Haji <hajial@gmail.com>
2020-10-17 13:25:06 -0400

    Replaces my hand-rolled gitignore with gitignore.io.

--- git/git.sh
+++ git/git.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env sh
-
-SCRIPT_DIR="$(cd "$(dirname $0)" && pwd)"
-DOTFILES_DIR="$(cd "$(dirname $SCRIPT_DIR)" && pwd)"
-DEST_DIR=$HOME
+#
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+DOTFILES_DIR="$(cd "$(dirname "$SCRIPT_DIR")" && pwd)"
+DEST_DIR="$HOME"
 
 . "$DOTFILES_DIR/utils.sh"
 
 create_symlink_and_backup "$SCRIPT_DIR/gitconfig" "$DEST_DIR/.gitconfig"
-create_symlink_and_backup "$SCRIPT_DIR/gitignore" "$DEST_DIR/.gitignore"
+
+# Could technically filter out the linux/mac/windows from this.
+# Probably not worth the effort.
+GITIGNORE_SOURCES=(
+    "linux"
+    "macos"
+    "windows"
+
+    "intellij"
+    "git"
+    "sublimetext"
+    "vim"
+    "vscode"
+    "zsh"
+)
+
+curl -fsSL "https://www.toptal.com/developers/gitignore/api/$(echo "${GITIGNORE_SOURCES[*]}" | sed 's/ /,/g')" > "$DEST_DIR/.gitignore"

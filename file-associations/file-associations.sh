029196bb7592b026300904b85c62b890ee06b326
Aleem Haji <hajial@gmail.com>
2021-01-25 23:13:02 -0500

    Create linux home config dir if it doesn't already exist.

--- file-associations/file-associations.sh
+++ file-associations/file-associations.sh
@@ -7,6 +7,7 @@ DOTFILES_DIR="$(cd "$(dirname $SCRIPT_DIR)" && pwd)"
 . "$DOTFILES_DIR/utils.sh"
 
 if [ "$(uname)" = "Linux" ]; then
+	mkdir -p "$HOME/.config"
 	create_symlink_and_backup "$DOTFILES_DIR/file-associations/mimeapps.list" "$HOME/.config/mimeapps.list"
 elif [ "$(uname)" = "Darwin" ]; then
 	duti "$SCRIPT_DIR/duti"

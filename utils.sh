8c8535d70519e8a9e07d5ab6d5d1d9bc6df2720f
Aleem Haji <hajial@gmail.com>
2022-04-02 23:23:53 -0400

    Fix some quoting on utils.sh, and backup file naming.

--- utils.sh
+++ utils.sh
@@ -2,8 +2,7 @@
 #   process.
 # Each function should do a decent job of documenting its use.
 
-create_symlink_and_backup()
-{
+create_symlink_and_backup() {
 	if [ -z "$1" ]; then
 		echo >&2 "usage: create_symlink_and_backup SOURCE DEST"
 		return 1
@@ -14,15 +13,15 @@ create_symlink_and_backup()
 		return 1
 	fi
 
-	source="$(cd $(dirname "$1") && pwd)/$(basename "$1")"
-	dest="$(cd $(dirname "$2") && pwd)/$(basename "$2")"
+	source="$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
+	dest="$(cd "$(dirname "$2")" && pwd)/$(basename "$2")"
 
     if [ -f "$dest" ] || [ -L "$dest" ]; then
         if [ "$(readlink -- "$dest")" = "$source" ]; then
             return 0
         fi
 
-        n_baks=$(find $(dirname "$dest") -iname "$(basename "$dest").bak.*" | wc -l)
+        n_baks="$(find "$(dirname "$dest")" -iname "$(basename "$dest").bak.*" | wc -l | awk '{print $1}')"
 
         mv "$dest" "$dest.bak.$n_baks"
     fi

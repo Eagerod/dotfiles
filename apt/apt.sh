068153484bd2a291549af2b917e4400df4659e3f
Aleem Haji <hajial@gmail.com>
2022-04-04 19:49:30 -0400

    Move docker installation to other apt-installed packages.

--- apt/apt.sh
+++ apt/apt.sh
@@ -60,6 +60,19 @@ sudo apt-get install -y \
     xclip \
     xz-utils
 
+# Docker
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/debian \
+    $(lsb_release -cs) \
+    stable"
+
+sudo apt-get update
+sudo apt-get install -y \
+    docker-ce \
+    docker-ce-cli \
+    containerd.io
+
 # Hop into a temp dir before trying to download installer packages; the user
 #   may not be allowed to write to the current directory.
 temp_dir="$(mktemp -d)"
@@ -125,18 +138,7 @@ else
     echo >&2 "Not installing Golang on this machine"
 fi
 
-# Docker
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo add-apt-repository \
-    "deb [arch=amd64] https://download.docker.com/linux/debian \
-    $(lsb_release -cs) \
-    stable"
 
-sudo apt-get update
-sudo apt-get install -y \
-    docker-ce \
-    docker-ce-cli \
-    containerd.io
 
 # Themeing
 sudo apt-get install -y \
